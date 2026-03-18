781898614ba763effe7e79edd99121221e9ddca0
Uday Sairam Kommineni <67004722+uday79936@users.noreply.github.com>
2026-03-18 10:42:16 +0530

    Update Product.java

--- src/main/java/com/store/model/Product.java
+++ src/main/java/com/store/model/Product.java
@@ -1,17 +1,72 @@
 package com.store.model;
 
+import java.util.Objects;
+
 public class Product {
     private int id;
     private String name;
     private double price;
 
+    // Default Constructor
+    public Product() {
+    }
+
+    // Parameterized Constructor
     public Product(int id, String name, double price) {
         this.id = id;
         this.name = name;
         this.price = price;
     }
 
-    public int getId() { return id; }
-    public String getName() { return name; }
-    public double getPrice() { return price; }
+    // Getters
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    // Setters
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    // toString()
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
+
+    // equals() and hashCode()
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Product)) return false;
+        Product product = (Product) o;
+        return id == product.id &&
+               Double.compare(product.price, price) == 0 &&
+               Objects.equals(name, product.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, price);
+    }
 }
